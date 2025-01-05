830a9dfbe96cfc70f4df138524efa45d260bd868
Adrian Joost <adi.joost@gmail.com>
2025-01-05 11:58:27 +0100

    robusted

--- storeArima.py
+++ storeArima.py
@@ -8,28 +8,32 @@ import warnings
 warnings.filterwarnings("ignore")
 
 csvSaveName = "arimaStatistics.csv"
+failedCSVNames = "failedCSV.csv"
 
 def main():
     createCSV()
     pairs = utils.loadStoreDepartmentPairs("longUniqueTimeSeries.csv")
     df = pd.read_csv("data/train.csv")
     for pair in tqdm(pairs):
-        analyse(pair, df)
+        try:
+            analyse(pair, df)
+        except Exception as e:
+            saveFailesCSV(pair[0], pair[1])
 
 def createCSV():
     with open(csvSaveName, "w", newline="", encoding="utf-8") as file:
         writer = csv.writer(file)
         writer.writerow(["storeId", "departmentId", "ma", "ar", "nDiff", "shapiroStat", "ShapiroValue", "ljungStat", "ljungValue"])
+    with open(failedCSVNames, "w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(["storeId", "departmentId"])
 
 def analyse(pair, df):
     storeTrain, storeTest = utils.getStoreDF(pair[0], pair[1], df, 0)
     diff, n = utils.getStationaryValue(storeTrain)
-    ma = max(utils.getACF(diff, nlags=52, confidence=0.25))
-    ar = max(utils.getPACF(diff, nlags=52, confidence=0.25))
+    ma = max(utils.getACF(diff, nlags=52, confidence=0.3))
+    ar = max(utils.getPACF(diff, nlags=52, confidence=0.3))
     model = utils.getArimaFit(diff, ar=ar, nDiff=n, ma=ma)
-    PreviousWeekly = storeTrain["Weekly_Sales"]
-    lastDate = storeTrain["Date"].max()
-    forecast = utils.getForecast(model, (PreviousWeekly,), lastDate, 12)
     residuals = model.resid
     shapiro_test_statistic, p_value = shapiro(residuals)
     ljung_box_test = acorr_ljungbox(residuals, lags=[ar], return_df=True)
@@ -41,6 +45,11 @@ def saveCSV(storeId, departmentId, ma, ar, nDiff, shapiroTestStatistic, shapiroP
     with open(csvSaveName, mode='a', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
         writer.writerow([storeId, departmentId, ma, ar, nDiff, shapiroTestStatistic, shapiroPValue, ljungStat, ljungPvalue ])
+
+def saveFailesCSV(storeId, departmentId):
+    with open(failedCSVNames, mode='a', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow([storeId, departmentId])
 
 if __name__ == "__main__":
     main()
